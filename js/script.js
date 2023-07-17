15ab817447b27d3193c3ae2f9940ff6c5ea2ca97
galihls <galih.angga524@gmail.com>
2023-07-17 22:06:25 +0700

    website kopi kenangan

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// // toggle class active
+
+// const navbarNav = document.querySelector(".navbar-nav");
+// //ketika hamburger menu di klik
+// document.querySelector("#hamburger-menu").oneclick = () => {
+//   navbarNav.classList.toggle("active");
+// };
+
+// Togglec class active
+
+const navbarNav = document.querySelector(".navbar-nav");
+//ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
